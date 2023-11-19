b6533d1d8290eaa21a1618acfce3470fd64914f7
lrubio013 <142952845+lrubio013@users.noreply.github.com>
2023-11-18 23:45:27 -0600

    added the questions for my application

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const inquirer = require("inquirer");
-const {Circle, Trianle, Square} = require("./lib/shapes");
+const {Circle, Triangle, Square} = require("./lib/shapes");
 const Svg = require("./lib/svg");
+const { readFile, writeFile } = require("fs").promises
+
+const question = [
+{
+    type: "input",
+    name: "text",
+    message: "Type 3 or less letters for your logo"
+},
+
+{
+    type: "input",
+    name: "textColor",
+    message: "What color would you like your text to be?"
+},
+
+{
+    type: "list",
+    name: "shape",
+    message: "Select a shape for the logo:",
+    choices: ["Circle", "Triangle", "Square"]
+},
+
+{
+    type: "input",
+    name: "shapeColor",
+    message: "What color would you like your shape to be?"
+},
+]
